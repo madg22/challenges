9a1ff0ee8d316f70d3f63e48923362646758a5a4
madg22 <98329454+madg22@users.noreply.github.com>
2025-05-27 18:18:38 -0500

    Update index.php

--- challenge-01/index.php
+++ challenge-01/index.php
@@ -1,1 +1,17 @@
+<?php
+
+function findPoint($strArr)
+{
+    // Convertir las cadenas a arrays de números, quitando espacios
+    $arr1 = array_map('trim', explode(',', $strArr[0]));
+    $arr2 = array_map('trim', explode(',', $strArr[1]));
+
+    $intersection = array_intersect($arr1, $arr2);
+
+    if (empty($intersection)) {
+        return "false";
+    }
+    return implode(',', $intersection);
+}
+echo findPoint(['1, 3, 4, 7, 13', '1, 2, 4, 13, 15']);
 
